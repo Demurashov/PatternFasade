cc7a1b615b72a142d52dcde0e8b057bc0303e662
Demurashov <denmurolimp@yandex.ru>
2023-01-25 17:48:22 +0300

    V1

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps binOps = new BinOps();
+        System.out.println("Суммирование: " + binOps.sum("111", "111"));
+        System.out.println("Умножение: " + binOps.mult("111", "111"));
+    }
+}
